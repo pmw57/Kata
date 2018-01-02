9c687524954065c1385db8ea93c16024f74c3004
Paul Wilkins <paul.m.wilkins@gmail.com>
2018-01-03 12:31:10 +1300

    Testing 3, 6, and 13 takes care of Foo requirements

--- foobarqix.js
+++ foobarqix.js
@@ -1,11 +1,14 @@
 /*jslint browser*/ /*global window*/
 window.foobarqix = function foobarqix(value) {
     "use strict";
+    if (typeof value !== "string") {
+        return foobarqix(String(value));
+    }
     var str = "";
     if (value % 3 === 0) {
         str = "Foo";
     }
-    if (value === "3") {
+    if (value.includes("3")) {
         str += "Foo";
     }
     if (!str) {
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -15,4 +15,5 @@
     expect(foobarqix("1"), "1", "1 doesn't match any critera so comes out unchanged");
     expect(foobarqix("3"), "FooFoo", "3 is divisible by 3, and contains 3");
     expect(foobarqix("6"), "Foo", "6 is divisible by 3");
+    expect(foobarqix("13"), "Foo", "13 contains a 3");
 }());
