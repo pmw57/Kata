5bba32c4982ae799c66af820efe29cfcae934179
Paul Wilkins <paul.m.wilkins@gmail.com>
2018-01-03 10:48:29 +1300

    Tests for 3 and 6 let us refactor for the common divisor.

--- foobarqix.js
+++ foobarqix.js
@@ -1,6 +1,15 @@
 /*jslint browser*/ /*global window*/
 window.foobarqix = function foobarqix(value) {
     "use strict";
-    // add code in here
-    return value;
+    var str = "";
+    if (value % 3 === 0) {
+        str = "Foo";
+    }
+    if (value === "3") {
+        str += "Foo";
+    }
+    if (!str) {
+        return value;
+    }
+    return str;
 };
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -13,4 +13,6 @@
 
     // place tests here
     expect(foobarqix("1"), "1", "1 doesn't match any critera so comes out unchanged");
+    expect(foobarqix("3"), "FooFoo", "3 is divisible by 3, and contains 3");
+    expect(foobarqix("6"), "Foo", "6 is divisible by 3");
 }());
