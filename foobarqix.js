16337b5464c17113fb9e3f91b331a8a0bc1879cf
Paul Wilkins <paul.m.wilkins@gmail.com>
2018-01-03 14:22:29 +1300

    Use map and factor to prevent foreach from mutating variables

--- foobarqix.js
+++ foobarqix.js
@@ -1,23 +1,28 @@
 /*jslint browser es6*/ /*global window*/
 window.foobarqix = function foobarqix(value) {
     "use strict";
+    function matchingFactor(digit, factorNames) {
+        return factorNames.filter(function ([factor]) {
+            return factor === digit;
+        }).map(function ([ignore, name]) {
+            return name;
+        });
+    }
+
     if (typeof value !== "string") {
         return foobarqix(String(value));
     }
-    var str = "";
     var factorNames = [["3", "Foo"], ["5", "Bar"]];
-    factorNames.forEach(function ([factor, name]) {
-        if (value % factor === 0) {
-            str += name;
-        }
-    });
-    factorNames.forEach(function ([factor, name]) {
-        if (value.includes(factor)) {
-            str += name;
-        }
-    });
-    if (!str) {
+    var str = factorNames.filter(function ([factor]) {
+        return value % factor === 0;
+    }).map(function ([ignore, name]) {
+        return name;
+    }).join("");
+    var suffix = value.split("").map(function (digit) {
+        return matchingFactor(digit, factorNames);
+    }).join("");
+    if (!str && !suffix) {
         return value;
     }
-    return str;
+    return str + suffix;
 };
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -19,4 +19,6 @@
     expect(foobarqix("5"), "BarBar", "5 is divisible by 5 and contains 5");
     expect(foobarqix("10"), "Bar", "10 is divisible by 5");
     expect(foobarqix("15"), "FooBarBar", "15 is divisible by 3 and 5, and contains 5");
+    expect(foobarqix("51"), "FooBar", "51 is divisible by 3 and contains 5");
+    expect(foobarqix("53"), "BarFoo", "53 contains 5 and 3");
 }());
