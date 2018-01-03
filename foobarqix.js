f2aef92d490750b1942e9fc3a38c242a5a5bf54d
Paul Wilkins <paul.m.wilkins@gmail.com>
2018-01-03 13:41:54 +1300

    5, 10, and 15 take care of the Bar requirements

--- foobarqix.js
+++ foobarqix.js
@@ -1,16 +1,21 @@
-/*jslint browser*/ /*global window*/
+/*jslint browser es6*/ /*global window*/
 window.foobarqix = function foobarqix(value) {
     "use strict";
     if (typeof value !== "string") {
         return foobarqix(String(value));
     }
     var str = "";
-    if (value % 3 === 0) {
-        str = "Foo";
-    }
-    if (value.includes("3")) {
-        str += "Foo";
-    }
+    var factorNames = [["3", "Foo"], ["5", "Bar"]];
+    factorNames.forEach(function ([factor, name]) {
+        if (value % factor === 0) {
+            str += name;
+        }
+    });
+    factorNames.forEach(function ([factor, name]) {
+        if (value.includes(factor)) {
+            str += name;
+        }
+    });
     if (!str) {
         return value;
     }
--- foobarqix.test.js
+++ foobarqix.test.js
@@ -16,4 +16,7 @@
     expect(foobarqix("3"), "FooFoo", "3 is divisible by 3, and contains 3");
     expect(foobarqix("6"), "Foo", "6 is divisible by 3");
     expect(foobarqix("13"), "Foo", "13 contains a 3");
+    expect(foobarqix("5"), "BarBar", "5 is divisible by 5 and contains 5");
+    expect(foobarqix("10"), "Bar", "10 is divisible by 5");
+    expect(foobarqix("15"), "FooBarBar", "15 is divisible by 3 and 5, and contains 5");
 }());
